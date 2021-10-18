c063704889f2882e04677fe3b801675a9371f18f
manasisarnaik <manasisarnaik97@gmail.com>
2021-10-18 11:15:49 +0530

    second function

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,36 +2,26 @@ package calculator;
 
 class StringCalculator {
 
-//	 public int add(String input) 
-//   {
-//   	int a = 0; int b = 0; int sum = 0;
-//   	StringTokenizer st = new StringTokenizer(input,",");
-//   	if(st.hasMoreTokens())
-//   	{
-//   		String temp = st.nextToken();
-//   		try
-//   		{
-//   			a = Integer.parseInt(temp);
-//   		}
-//   		catch(Exception e)
-//   		{
-//   			a = 0;
-//   		}
-//   	}
-//   	if(st.hasMoreTokens())
-//   	{
-//   		String temp = st.nextToken();
-//   		try
-//   		{
-//   			b = Integer.parseInt(temp);
-//   		}
-//   		catch(Exception e)
-//   		{
-//  			b = 0;
-//   		}
-//   	}
-//   	sum = a + b;
-////   	System.out.println(input+"  "+sum);
-//       return sum;
-//   }
+//  public int add(String input) 
+//  {
+//  	int sum = 0;
+//  	StringTokenizer st = new StringTokenizer(input,",");
+//  	while(st.hasMoreTokens())
+//  	{
+//  		int a = 0;
+//  		String temp = st.nextToken();
+//  		try
+//  		{
+//  			a = Integer.parseInt(temp);
+//  		}
+//  		catch(Exception e)
+//  		{
+//  			a = 0;
+//  		}
+//  		sum = sum + a;
+//  	}
+//  	
+//  	System.out.println(input+"  "+sum);
+//      return sum;
+//  }
 }
